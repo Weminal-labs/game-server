0ac098bfa707f9e760d1046c7eb3f6073f4db5ca
tvh050423@gmail.com <tvh050423@gmail.com>
2024-04-03 15:35:13 +0700

    feat: testing connection of game and nextjs

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className='w-full min-h-screen'>
+    <div className='w-full min-h-screen flex justify-center items-center'>
       {children}
     </div>
   )
